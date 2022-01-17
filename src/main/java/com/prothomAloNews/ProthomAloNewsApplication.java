b8ea27f091f36917d821f2c7956614105f337974
Ahanab Shakil <shieam47@gmail.com>
2022-01-17 20:50:22 +0600

    Project Initialized

---
+++ src/main/java/com/prothomAloNews/ProthomAloNewsApplication.java
@@ -0,0 +1,13 @@
+package com.prothomAloNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProthomAloNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProthomAloNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prothomAloNews/ProthomAloNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prothomAloNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProthomAloNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
